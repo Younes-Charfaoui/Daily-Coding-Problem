be38b919b21846b750bc53752c898639791aa10a
Younes Charfaoui <mxcsyounes@ggmail.com>
2018-07-23 21:00:04 +0100

    Google Problem: no solution yet #4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Daily Coding Problem
 A solutions series of problem asked by Daily Coding problem website.
+
+#Contributing
---
+++ google_problems/problem_two.py
@@ -0,0 +1,15 @@
+"""This problem was asked by Google.
+
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'"""
