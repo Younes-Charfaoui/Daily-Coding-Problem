cab5233530e19b2800c81f7b493747473996171d
Younes Charfaoui <mxcsyounes@gmail.com>
2018-07-29 19:57:31 +0100

    Airbnb Problem #1 : No solution yet.

---
+++ Airbnb/problem_one.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Airbnb.
+Given a list of integers, write a function that returns the largest sum of
+non-adjacent numbers. Numbers can be 0 or negative.
+For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5.
+[5, 1, 1, 5] should return 10, since we pick 5 and 5.
+Follow-up: Can you do this in O(N) time and constant space?
+"""
