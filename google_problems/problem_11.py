1aa8232ec00cfaa4347d7a5349981fe00abfc355
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-15 20:21:46 +0100

    Google problem #11: like always, no solution #26

---
+++ google_problems/problem_11.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Google.
+Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
+The list is very long, so making more than one pass is prohibitively expensive.
+Do this in constant space and in one pass.
+"""
