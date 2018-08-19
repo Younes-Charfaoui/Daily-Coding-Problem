0c956c49b35540f16816b74196219e1114e79ab9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-19 19:34:20 +0100

    Facebook problem #6: basic solution #32

--- facebook_problems/problem_6.py
+++ facebook_problems/problem_6.py
@@ -1,6 +1,39 @@
 """This problem was asked by Facebook.
-You are given an array of non-negative integers that represents a two-dimensional elevation map where each element is unit-width wall and the integer is the height. Suppose it will rain and all spots between two walls get filled up.
+You are given an array of non-negative integers that represents a two-dimensional
+elevation map where each element is unit-width wall and the integer is the height.
+Suppose it will rain and all spots between two walls get filled up.
 Compute how many units of water remain trapped on the map in O(N) time and O(1) space.
 For example, given the input [2, 1, 2], we can hold 1 unit of water in the middle.
-Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
+Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index,
+2 in the second, and 3 in the fourth index (we cannot hold 5 since it
+would run off to the left), so we can trap 8 units of water.
 """
+
+
+def main(array):
+    if len(array) < 3:
+        return 0
+    start = array[0]
+    end = array[len(array) - 1]
+
+    if start < end:
+        small = start
+    else:
+        small = end
+
+    output = 0
+    if start == end:
+        for i in array:
+            value = start - i
+            if value > 0:
+                output += value
+    else:
+        for i in array:
+            value = small - i
+            if value > 0:
+                output += value
+    return output
+
+
+print(main([2, 1, 2]))
+print(main([3, 0, 1, 3, 0, 5]))
