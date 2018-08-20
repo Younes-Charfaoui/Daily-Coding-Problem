76eec26febdedde99fedf7fd3ab0b6f3b2b49d78
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-20 18:09:38 +0100

    Google Problem #12: no solution #33

---
+++ google_problems/problem_12.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Google.
+The edit distance between two strings refers to the minimum 
+number of character insertions, deletions, and substitutions 
+required to change one string to the other. For example, 
+the edit distance between “kitten” and “sitting” is three: 
+substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+Given two strings, compute the edit distance between them."""
