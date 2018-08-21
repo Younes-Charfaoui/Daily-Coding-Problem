0ac6f4e6f270d0b26a81dfe1414a658dd3b17b34
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-21 18:18:04 +0100

    Jane street problem #2: no solution, Amazon problem #2: finaly got a solution #35

--- amazon_problems/problem_2.py
+++ amazon_problems/problem_2.py
@@ -1,7 +1,39 @@
 """This problem was asked by Amazon.
 
 Given an integer k and a string s, find the length of the longest substring
- that contains at most k distinct characters.
+that contains at most k distinct characters.
 
 For example, given s = "abcba" and k = 2, the longest substring with k
 distinct characters is "bcb"."""
+
+
+def main(s, k):
+    start = 0
+    current = ""
+    max_lengths = 0
+    while start < len(s):
+        for i in range(start + k - 1, len(s)):
+            ss = s[start:i + 1]
+            counter = count_character(ss)
+            if counter > k:
+                break
+            else:
+                if len(ss) > max_lengths:
+                    max_lengths = len(ss)
+                    current = ss
+        start += 1
+    return current
+
+
+def count_character(string):
+    current = [string[0]]
+    counter = 1
+    for i in string:
+        if i not in current:
+            counter += 1
+            current.append(i)
+    return counter
+
+
+# print(count_character("abbbcaabca"))
+print(main("abbbcef", 3))
