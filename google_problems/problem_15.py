ca75db7ee84f393006d6dc28b0366b6a20031879
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-29 18:03:17 +0100

    Google problem #15: no solution yet #42

---
+++ google_problems/problem_15.py
@@ -0,0 +1,9 @@
+"""This problem was asked by Google.
+
+Given an array of integers where every integer occurs three times except
+ for one integer, which only occurs once, find and return the non-duplicated integer.
+
+For example, given [6, 1, 3, 3, 3, 6, 6], 
+return 1. Given [13, 19, 13, 13], return 19.
+
+Do this in O(N) time and O(1) space."""
