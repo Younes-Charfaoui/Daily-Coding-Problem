760118b04af70c1c0e8776001f1b030e8fb72b57
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-04 21:26:52 +0100

    Amazon problem #5: no solution #49

---
+++ amazon_problems/problem_5.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Amazon.
+
+Given a string, find the longest palindromic contiguous substring. If there
+are more than one with the maximum length, return any one.
+
+For example, the longest palindromic substring of "aabcdcb" is "bcdcb". 
+The longest palindromic substring of "bananas" is "anana".
+"""
