c0ea7045b7af4d3c0d664ba2750e14f0aa73cde9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-05 17:20:10 +0100

    Facebook problem #8: did not understand the problem #50

---
+++ facebook_problems/problem_8.py
@@ -0,0 +1,10 @@
+"""This problem was asked by Facebook.
+
+Given a array of numbers representing the stock prices of 
+a company in chronological order, write a function that calculates 
+the maximum profit you could have made from buying 
+and selling that stock once. You must buy before you can sell it.
+
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, since 
+you could buy the stock at 5 dollars and sell it at 10 dollars.
+"""
