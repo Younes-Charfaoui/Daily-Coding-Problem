4043196b83146b17b9462be5fbef70a46e6cfd26
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-09 18:19:22 +0100

    Facebook Problem #9: Hahahahahaha -_- #54

---
+++ facebook_problems/problem_9.py
@@ -0,0 +1,7 @@
+"""Given a function that generates perfectly random numbers 
+between 1 and k (inclusive), where k is an input, write a function
+that shuffles a deck of cards represented as an array using only swaps.
+It should run in O(N) time.
+Hint: Make sure each one of the 52! 
+permutations of the deck is equally likely.
+"""
