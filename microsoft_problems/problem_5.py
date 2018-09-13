d0d396d49ca5f5b8a77f50cf1f4d3a51df1faa67
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-13 18:40:55 +0100

    Microsoft Problem #5: No solution yet #58

---
+++ microsoft_problems/problem_5.py
@@ -0,0 +1,9 @@
+"""Good morning! Here's your coding interview problem for today.
+This problem was asked by Microsoft.
+Implement a URL shortener with the following methods:
+•	shorten(url), which shortens the url into a six-character 
+alphanumeric string, such as zLg6wl.
+•	restore(short), which expands the shortened string into the 
+original url. If no such shortened string exists, return null.
+Hint: What if we enter the same URL twice?
+"""
