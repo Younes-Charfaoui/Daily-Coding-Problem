5d068cd84f915183f8cecdaf6552d76901bcf368
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-16 17:44:33 +0100

    Amazon Problem #8: like always, this one is tricky #61

---
+++ amazon_problems/problem_8.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Amazon.
+An sorted array of integers was rotated an unknown number of times.
+Given such an array, find the index of the element in the array in 
+faster than linear time. If the element doesn't exist in the array, return null.
+For example, given the array [13, 18, 25, 2, 8, 10] and the 
+element 8, return 4 (the index of 8 in the array).
+You can assume all the integers in the array are unique."""
