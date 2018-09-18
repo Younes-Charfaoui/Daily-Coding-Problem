8391ce9fed0450cb41ae921c93fb7f16bd28f2d3
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-18 21:26:56 +0100

    Facebook Problem #10 : no solution yet #63

---
+++ facebook_problems/problem_10.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Facebook.
+
+Given a multiset of integers, return whether it can be partitioned into 
+two subsets whose sums are the same.
+
+For example, given the multiset {15, 5, 20, 10, 35, 15, 10}, it would return true, 
+since we can split it up into {15, 5, 10, 15, 10} and {20, 35}, which both add up to 55.
+
+Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't 
+split it up into two subsets that add up to the same sum
+"""
