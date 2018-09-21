a53b6473280651171f805e846c181f9493da94d5
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-21 17:48:11 +0100

    Microsoft Peoblem #7 : no solution yet  #68

---
+++ microsoft_problems/problem_6.py
@@ -0,0 +1,12 @@
+"""This problem was asked by Microsoft.
+Given a 2D matrix of characters and a target word, write a function that returns
+whether the word can be found in the matrix by going left-to-right, or up-to-down.
+For example, given the following matrix:
+[['F', 'A', 'C', 'I'],
+ ['O', 'B', 'Q', 'P'],
+ ['A', 'N', 'O', 'B'],
+ ['M', 'A', 'S', 'S']]
+and the target word 'FOAM', you should return true, since it's the leftmost column. 
+Similarly, given the target word 'MASS', 
+you should return true, since it's the last row.
+"""
