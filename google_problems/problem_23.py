3b839d269c5d37791698c1f151a50ed3ff8f89a7
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-22 18:56:05 +0100

    Google Problem #69: No solution yet #69

---
+++ google_problems/problem_23.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Google.
+
+A knight's tour is a sequence of moves by a knight on a 
+chessboard such that all squares are visited once.
+
+Given N, write a function to return the number 
+of knight's tours on an N by N chessboard.
+"""
