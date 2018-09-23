c78f1c7c3027478cd224288e6ddb046fd655c6d4
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-24 00:33:32 +0100

    Amazon Problem #9 : First attempt , but failed #72

--- amazon_problems/problem_9.py
+++ amazon_problems/problem_9.py
@@ -43,7 +43,35 @@ def main():
     ce = len(matrix[0]) - 1
     ci = 0
     cj = 0
+    total = len(matrix) * len(matrix[0])
+    now = 0
+    ori = 0
+    while now < total:
+        if ori == 0:
+            for cj in range(cs, ce):
+                print(matrix[ci][cj])
+                now += 1
+            ce -= 1
+            ori = 1
+        elif ori == 1:
+            for ci in range(rs, re):
+                print(matrix[ci][cj])
+                now += 1
+            rs += 1
+            ori = 2
 
+        elif ori == 2:
+            for cj in range(ce, cs, -1):
+                print(matrix[ci][cj])
+                now += 1
+            cs += 1
+            ori = 3
+        elif ori == 3:
+            for ci in range(re, rs, -1):
+                print(matrix[ci][cj])
+                now += 1
+            re -= 1
+            ori = 0
 
 
 main()
