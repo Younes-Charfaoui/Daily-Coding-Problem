8205163042c1f84592b44495344401ba2b82d26b
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-24 18:44:08 +0100

    Square Problem #1 : Retrofit Players Problem hahaha , no solution yet #73

---
+++ square_problems/problem_1.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Square.
+Assume you have access to a function toss_biased() which returns 0 or 1 with
+a probability that's not 50-50 (but also not 0-100 or 100-0). 
+You do not know the bias of the coin.
+Write a function to simulate an unbiased coin toss.
+"""
