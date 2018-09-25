ee054a8112501c7cbd56283c9901c4f0dad33dd9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-25 01:03:25 +0100

    Amazon Problem #9 : Solved Finally Hahaha #74

--- amazon_problems/problem_9.py
+++ amazon_problems/problem_9.py
@@ -46,32 +46,37 @@ def main():
     total = len(matrix) * len(matrix[0])
     now = 0
     ori = 0
+
     while now < total:
+
         if ori == 0:
-            for cj in range(cs, ce):
-                print(matrix[ci][cj])
-                now += 1
-            ce -= 1
-            ori = 1
-        elif ori == 1:
-            for ci in range(rs, re):
+            for cj in range(cs, ce + 1):
                 print(matrix[ci][cj])
                 now += 1
             rs += 1
+            ori = 1
+
+        elif ori == 1:
+            for ci in range(rs, re + 1):
+                print(matrix[ci][cj])
+                now += 1
+            ce -= 1
             ori = 2
 
         elif ori == 2:
-            for cj in range(ce, cs, -1):
-                print(matrix[ci][cj])
-                now += 1
-            cs += 1
-            ori = 3
-        elif ori == 3:
-            for ci in range(re, rs, -1):
+            for cj in range(ce, cs - 1, -1):
                 print(matrix[ci][cj])
                 now += 1
             re -= 1
+            ori = 3
+
+        elif ori == 3:
+            for ci in range(re, rs-1, -1):
+                print(matrix[ci][cj])
+                now += 1
+            cs += 1
             ori = 0
+
 
 
 main()
