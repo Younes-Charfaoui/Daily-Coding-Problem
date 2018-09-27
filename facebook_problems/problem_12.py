65e094d0b41dc0e948d69af3e4f5476cebdf81bd
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-27 19:40:05 +0100

    Facebook Problem #12: No Solution yet #81

---
+++ facebook_problems/problem_12.py
@@ -0,0 +1,8 @@
+"""Good morning! Here's your coding interview problem for today.
+This problem was asked by Facebook.
+Given a list of integers, return the largest product that can be made 
+by multiplying any three integers.
+For example, if the list is [-10, -10, 5, 2], we should return 500, since that's -10 * -10 * 5.
+You can assume the list has at least three integers.
+
+"""
