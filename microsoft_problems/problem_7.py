c71b3cc15e9b0f40ded3a0a4d09ea6c196a5735c
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-28 18:31:26 +0100

    Microsoft Problem #7 : Start with simple method of getting sum of digits #84

--- microsoft_problems/problem_7.py
+++ microsoft_problems/problem_7.py
@@ -2,3 +2,16 @@
 A number is considered perfect if its digits sum up to exactly 10.
 Given a positive integer n, return the n-th perfect number.
 For example, given 1, you should return 19. Given 2, you should return 28"""
+
+def make_perfect(number):
+	arr = str(number)
+	r = 0
+	for i in arr:
+		r+= int(i)
+	return arr+str()
+
+def sum_number(number):
+	if int(number) < 10 : 
+		return int(number)
+	else:
+		return int(number[0]) + sum_number(number[1:])
