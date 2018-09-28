0b2267de3ef1aa7d0965712daeebd0c7c3385ebd
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-28 18:15:46 +0100

    Microsoft Problem #7 : no solution yet #83

---
+++ microsoft_problems/problem_7.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Microsoft.
+A number is considered perfect if its digits sum up to exactly 10.
+Given a positive integer n, return the n-th perfect number.
+For example, given 1, you should return 19. Given 2, you should return 28"""
