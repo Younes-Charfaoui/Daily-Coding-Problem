71eead61850405bbae77f72f00e69aae826f5865
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-28 18:46:15 +0100

    Microsoft Problem #7 : Correcting the function of summing digits #85

--- microsoft_problems/problem_7.py
+++ microsoft_problems/problem_7.py
@@ -3,15 +3,28 @@ A number is considered perfect if its digits sum up to exactly 10.
 Given a positive integer n, return the n-th perfect number.
 For example, given 1, you should return 19. Given 2, you should return 28"""
 
+
 def make_perfect(number):
-	arr = str(number)
-	r = 0
-	for i in arr:
-		r+= int(i)
-	return arr+str()
+    arr = str(number)
+    r = 0
+    for i in arr:
+        r += int(i)
+    return arr + str()
+
 
 def sum_number(number):
-	if int(number) < 10 : 
-		return int(number)
-	else:
-		return int(number[0]) + sum_number(number[1:])
+    if int(number) <= 10:
+        return int(number)
+    else:
+        return sum_number(count(number))
+
+
+def count(number):
+    arr = str(number)
+    r = 0
+    for i in arr:
+        r += int(i)
+    return str(r)
+
+
+print(sum_number("991"))
