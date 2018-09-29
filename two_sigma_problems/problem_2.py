35ab83b79d203f751845e389d4d615add9832e2f
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-29 19:22:00 +0100

    Two Sigma Problem #2: Two Sigma Is Kidding Again #88

---
+++ two_sigma_problems/problem_2.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Two Sigma.
+Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, 
+implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+"""
