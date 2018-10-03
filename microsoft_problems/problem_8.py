8d3a772e7d4165d23fc7dabb9dcc2f72ce891f6d
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-03 18:25:50 +0100

    Microsoft Problem #7: just a nice problem , no solution yet #92

---
+++ microsoft_problems/problem_8.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Microsoft.
+Given an array of numbers, find the length of the longest increasing subsequence in the array. 
+The subsequence does not necessarily have to be contiguous.
+For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], 
+the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
+"""
