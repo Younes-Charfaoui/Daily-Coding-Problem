3ae8a70b3791a3038416c5725cc3f368f242e769
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-05 19:22:41 +0100

    Snapchat Problem #2 : They Are playing on Intervales :D #94

---
+++ snapchat_problems/problem_2.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Snapchat.
+Given a list of possibly overlapping intervals, return a new list of 
+intervals where all overlapping intervals have been merged.
+The input list is not necessarily ordered in any way.
+For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], 
+you should return [(1, 3), (4, 10), (20, 25)]
+"""
