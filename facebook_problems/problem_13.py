df5ecb25d3fa62d7a9eb8883c09b662fade1882f
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-07 17:48:28 +0100

    Facebook Problem #13 : no solution yet #96

---
+++ facebook_problems/problem_13.py
@@ -0,0 +1,8 @@
+"""Given an array of integers, write a function to determine 
+whether the array could become non-decreasing by modifying at most 1 element.
+
+For example, given the array [10, 5, 7], you should return true, since we 
+can modify the 10 into a 1 to make the array non-decreasing.
+
+Given the array [10, 5, 1], you should return false, since 
+we can't modify any one element to get a non-decreasing array."""
