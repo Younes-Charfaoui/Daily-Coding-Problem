7a380aac1abacde7b233312d443fcfb5e5c14d16
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-09 20:42:50 +0100

    Yelp Problem#1 : seems to be a simple one, not solution yet #98

---
+++ yelp_problems/problem_1.py
@@ -0,0 +1,6 @@
+"""Given a mapping of digits to letters (as in a phone number), and a digit string, 
+return all possible letters the number could represent. You can assume each 
+valid number in the mapping is a single digit.
+For example if {“2”: [“a”, “b”, “c”], 3: [“d”, “e”, “f”], …} 
+then “23” should return [“ad”, “ae”, “af”, “bd”, “be”, “bf”, “cd”, “ce”, “cf"].
+"""
