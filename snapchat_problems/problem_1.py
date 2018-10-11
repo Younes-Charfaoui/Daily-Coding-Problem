6750276474ff0ec89d90412ff780de27f2b7c9a1
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-11 18:12:18 +0100

    Snapchat Problem #1: simple and complex solution #100

--- snapchat_problems/problem_1.py
+++ snapchat_problems/problem_1.py
@@ -3,3 +3,37 @@ Given an array of time intervals (start, end) for classroom
 lectures (possibly overlapping), find the minimum number of rooms required.
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 """
+
+
+def find_small(array):
+    mina = array[0][0]
+    index = 0
+    for ind in range(len(array)):
+        if array[ind][0] < mina:
+            index = ind
+    return index
+
+
+def big_all(value, array):
+    result = True
+    for i in array:
+        if i > value:
+            result = False
+    return result
+
+
+intervals = [(30, 75), (0, 50), (60, 150)]
+
+mins = []
+for i in intervals:
+    if len(mins) == 0:
+        var = find_small(intervals)
+        mins.append(intervals[var][1])
+        del intervals[var]
+    else:
+        var = find_small(intervals)
+        if not big_all(intervals[var][0], mins):
+            mins.append(intervals[var][0])
+        del intervals[var]
+
+print(len(mins))
