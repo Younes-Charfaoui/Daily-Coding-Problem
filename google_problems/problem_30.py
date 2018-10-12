e7ead8604e14c506d5325c21a2a42edb4c1a350f
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-12 15:52:34 +0100

    Google Problem #30: not yet #103

---
+++ google_problems/problem_30.py
@@ -0,0 +1,19 @@
+"""This problem was asked by Google.
+
+Invert a binary tree.
+
+For example, given the following tree:
+
+    a
+   / \
+  b   c
+ / \\  /
+d   e f
+should become:
+
+  a
+ / \
+ c  b
+ \\  / \
+  f e  d
+"""
