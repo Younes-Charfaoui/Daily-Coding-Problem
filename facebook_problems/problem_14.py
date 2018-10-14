e183b368d070407db7233f18b340ddd1415f5100
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-14 22:14:22 +0100

    Facebook Problem #14 : no yet #105

---
+++ facebook_problems/problem_14.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Facebook.
+
+Given three integers x,y and b, return x if b is equal to 0, and y if b is equal to 1.
+
+Do this only using mathematical and bits operation.
+
+You can assume that b is 1 or 0."""
