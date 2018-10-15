7f927c4e058d7825af46ac360ec8474ae7ee7232
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-15 14:07:49 +0100

    Google Problem #31 : no yet like always #106

---
+++ google_problems/problem_31.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Google.
+Given a string of parentheses, write a function to compute the minimum number of parentheses to be 
+removed to make the string valid (i.e. each open parenthesis is eventually closed).
+For example, given the string "()())()", you should return 1. 
+Given the string ")(", you should return 2, since we must remove all of them."""
