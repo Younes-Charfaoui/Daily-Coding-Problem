d265b26cfabd5b599e5e4c7238160b3285de70b9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-17 22:54:41 +0100

    ContextLogic Problem #1 : just an easy problem #110

--- context_logic_problems/problem_1.py
+++ context_logic_problems/problem_1.py
@@ -2,3 +2,17 @@
 
 Implement division of two positive integers without using the division, 
 multiplication, or modulus operators. Return the quotient as an integer, ignoring the remainder."""
+
+# main method for making division, quiet easy: just subtract second nomber for the first until 0. and count.
+def division(a,b):
+    i=0
+    while a >= b:
+        i += 1
+        a = a-b
+    return i
+
+print(division(6,2))
+print(division(10,5))
+print(division(20,5))
+print(division(120,20))
+        
