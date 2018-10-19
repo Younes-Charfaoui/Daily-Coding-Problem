108332470923fb1d5ef32927b4fc6a80c5e5d8c1
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-19 23:28:19 +0100

    Linked In Problem #1 : Adding The tree Class #115

--- linked_in_problems/problem_1.py
+++ linked_in_problems/problem_1.py
@@ -5,3 +5,35 @@ Determine whether a tree is a valid binary search tree.
 A binary search tree is a tree with two children, left and right, and satisfies
 the constraint that the key in the left child must be less than or 
 equal to the root and the key in the right child must be greater than or equal to the root."""
+
+class Node:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.right = None
+        self.left = None
+
+
+class BinaryTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        node = Node(data)
+        if not self.root:
+            self.root = node
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        if data < node.data:
+            if node.left:
+                self.insert_node(data, node.left)
+            else:
+                node.left = Node(data)
+        else:
+            if node.right:
+                self.insert_node(data, node.right)
+            else:
+                node.right = Node(data)
