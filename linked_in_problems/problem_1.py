f5e721130e7b7cacc0ead3f57a6763b44cc21d17
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-19 16:55:12 +0100

    Linked In Problem #1: tree problems again -_- , not yet #114

---
+++ linked_in_problems/problem_1.py
@@ -0,0 +1,7 @@
+"""This problem was asked by LinkedIn.
+
+Determine whether a tree is a valid binary search tree.
+
+A binary search tree is a tree with two children, left and right, and satisfies
+the constraint that the key in the left child must be less than or 
+equal to the root and the key in the right child must be greater than or equal to the root."""
