39abf920ad54f35a49a4332d43c6d4a26088678b
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-24 20:17:46 +0100

    Microsoft Problem #10 : not yet , but it seems easy #122

---
+++ microsoft_problems/problem_10.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Microsoft.
+Given a number in the form of a list of digits, return all possible permutations.
+For example, given [1,2,3], return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]].
+"""
