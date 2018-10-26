21f41061aebd9c36af9c88891c2f61ab6825f9cb
Younes Charfaoui <mxcsyounes@gmail.com>
2018-10-26 19:13:27 +0100

    Coursera Problem #1 : not yet like always #124

---
+++ coursera_problems/problem_1.py
@@ -0,0 +1,12 @@
+"""This problem was asked by Coursera.
+Given a 2D board of characters and a word, find if the word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells 
+are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+For example, given the following board:
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+exists(board, "ABCCED") returns true, exists(board, "SEE") returns true, exists(board, "ABCB") returns false
+"""
