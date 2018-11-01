433c79686373c69b1cf1cd257497533383cd696b
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-01 17:14:46 +0100

    Google Problem #35 : not yet like always

---
+++ google_problems/problem_35.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Google.
+Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
+For example, 1 -> 4 -> 3 -> 4 -> 1 returns true while 1 -> 4 returns false.
+"""
