81517cbbce59773b6b31cab717dbd2ddd8be3392
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-02 17:11:36 +0100

    Facebook Problem #15: it's not understandable Hahaha

---
+++ facebook_problems/problem_15.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Facebook.
+Given a function f, and N return a debounced f of N milliseconds.
+That is, as long as the debounced f continues to be invoked, f itself will not be called for N milliseconds.
+"""
