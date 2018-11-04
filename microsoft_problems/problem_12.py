c239c4b4cef218c05ead8fe8ebf2144a3e770c16
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-04 20:08:13 +0100

    Microsoft Problem #12 : tree problems again , not yet

---
+++ microsoft_problems/problem_12.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Microsoft.
+
+Print the nodes in a binary tree level-wise. For example, the following should print
+1, 2, 3, 4, 5.
+
+  1
+ / \
+2   3
+   / \
+  4   5
+"""
