e62b32708ec3fa407cff49a3df7ec58f01351265
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-05 20:03:42 +0100

    Google Problem #36: not yet, but seems easy

---
+++ google_problems/problem_36.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Google.
+Given two strings A and B, return whether or not A can be shifted some number of times to get B.
+For example, if A is abcde and B is cdeab, return true. If A is abc and B is acb, return false.
+"""
