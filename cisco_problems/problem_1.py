58de2b578e5e24ed44a3e85c98ed50ef02ddd625
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-06 20:06:20 +0100

    Cisco Problem #1: it's cisco time now! not yet

---
+++ cisco_problems/problem_1.py
@@ -0,0 +1,9 @@
+"""This problem was asked by Cisco.
+
+Given an unsigned 8-bit integer, swap its even and odd bits. 
+The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.
+
+For example, 10101010 should be 01010101. 11100010 should be 11010001.
+
+Bonus: Can you do this in one line?
+"""
