138a9a1f8cf3741384befff1a431bb2c5972185d
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-07 19:02:52 +0100

    Facebook Problem #16: facebook are playing with trees now :D , not yet

---
+++ facebook_problems/problem_16.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Facebook.
+Given a binary tree, return all paths from the root to leaves.
+For example, given the tree
+   1
+  / \
+ 2   3
+    / \
+   4   5
+it should return [[1, 2], [1, 3, 4], [1, 3, 5]].
+
+"""
