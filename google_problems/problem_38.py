a18c982d8a42e4e342bb50ca73a2117213fedcba
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-10 20:58:57 +0100

    Google Problem #38: reversing problem agains , not yet -_-

---
+++ google_problems/problem_38.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Google.
+Given a string of words delimited by spaces, reverse the words in string. For example, 
+given "hello world here", return "here world hello"
+Follow-up: given a mutable string representation, can you perform this operation in-place?
+"""
