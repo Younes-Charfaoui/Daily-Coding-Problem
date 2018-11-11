c6d2aad8ec5f47141e6d3b63a1e1d3b71a3f257d
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-11 19:56:15 +0100

    Facebook Problem #17: seems all companies focused on String Reversing -_- , not yet

---
+++ facebook_problems/problem_17.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Facebook.
+
+Given a string and a set of delimiters, reverse the words in the string 
+while maintaining the relative order of the delimiters. For example, given "hello/world:here", return "here/world:hello"
+
+Follow-up: Does your solution work for the 
+following cases: "hello/world:here/", "hello//world:here"
+"""
