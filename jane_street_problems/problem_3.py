91d133488368d24cd5b3138b5b3640bf5e32ba52
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-13 18:13:29 +0100

    Jane Streep Problem #3: not yet honey

---
+++ jane_street_problems/problem_3.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Jane Street.
+Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+That is, generate() should return a tree whose size is unbounded but finite.
+"""
