7960837644de8ab8bd169cd633d92d8c956ce354
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-15 20:53:58 +0100

    Google Problem #40: nice one here, not yet

---
+++ google_problems/problem_40.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Google.
+
+Given a sorted list of integers, square the elements and give the output in sorted order.
+
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+"""
