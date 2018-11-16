f3a63568920075e33e7f6c215d071a86c73628a9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-16 19:02:49 +0100

    Google Problem #41: keep playing with numbers -_-

---
+++ google_problems/problem_41.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Google.
+
+Given a set of closed intervals, find the smallest set of 
+numbers that covers all the intervals. If there are multiple smallest sets, return any of them.
+
+For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], 
+one set of numbers that covers all these intervals is {3, 6}.
+"""
