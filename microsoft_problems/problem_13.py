92c54034a9901ab475c7e0d073afaa21cd7e9831
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-17 18:29:17 +0100

    Microsoft Problem #13: not yet, but today inshalah

---
+++ microsoft_problems/problem_13.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Microsoft.
+
+Implement the singleton pattern with a twist. First, instead of storing one instance, store two instances. 
+And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), 
+return the second instance.
+"""
