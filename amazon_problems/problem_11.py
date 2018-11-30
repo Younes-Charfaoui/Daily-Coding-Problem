37c7f72a661668fcda3205925825afcc3125bf0b
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-30 18:42:21 +0100

    Amazong Problem #11: not yet again and again.

---
+++ amazon_problems/problem_11.py
@@ -0,0 +1,13 @@
+"""This problem was asked by Amazon.
+
+Given a node in a binary tree, return the next bigger element, also known as the inorder successor.
+
+For example, the inorder successor of 22 is 30.
+
+   10
+  /  \
+ 5    30
+     /  \
+   22    35
+You can assume each node has a parent pointer.
+"""
