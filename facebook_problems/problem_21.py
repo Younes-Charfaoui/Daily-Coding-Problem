792e7510a37d13045a780e8d0d06c3243169d986
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-01 20:14:23 +0100

    Facebook Problem #21: finaly a space complexity problem but not yet -_-

---
+++ facebook_problems/problem_21.py
@@ -0,0 +1,10 @@
+"""This problem was asked by Facebook.
+
+You have a large array with most of the elements as zero.
+
+Use a more space-efficient data structure, SparseArray, that implements the same interface:
+
+init(arr, size): initialize with the original large array and size.
+set(i, val): updates index at i with val.
+get(i): gets the value at index i.
+"""
