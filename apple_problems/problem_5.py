3c3ec7db778a05e3646c3766bd3d12a9ff460278
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-02 22:05:31 +0100

    Apple Problem #5: not yet

---
+++ apple_problems/problem_5.py
@@ -0,0 +1,14 @@
+"""This question was asked by Apple.
+
+Given a binary tree, find a minimum path sum from root to a leaf.
+
+For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
+
+  10
+ /  \
+5    5
+ \\     \
+   2    1
+       /
+     -1
+"""
