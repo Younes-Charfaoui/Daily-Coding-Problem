7093b6e72dbd7fdaf43e87a98b8f9c96de7e031d
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-03 18:50:45 +0100

    Google Problem #44 : not yet honey, chikhaoui is making things hard for us

---
+++ google_problems/problem_44.py
@@ -0,0 +1,13 @@
+"""This question was asked by Google.
+
+Given an N by M matrix consisting only of 1's and 0's, find the largest rectangle containing only 1's and 
+return its area.
+
+For example, given the following matrix:
+
+[[1, 0, 0, 0],
+ [1, 0, 1, 1],
+ [1, 0, 1, 1],
+ [0, 1, 0, 0]]
+Return 4.
+"""
