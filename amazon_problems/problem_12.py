f102a277ef2e690ee8e283c28cde08802c5db7af
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-04 20:54:16 +0100

    Amazon Problem #12 : not yet

---
+++ amazon_problems/problem_12.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Amazon.
+Implement a bit array.
+A bit array is a space efficient array that holds a value of 1 or 0 at each index.
+•	init(size): initialize the array with size
+•	set(i, val): updates index at i with val where val is either 1 or 0.
+•	get(i): gets the value at index i.
+"""
