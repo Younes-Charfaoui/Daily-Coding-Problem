17cd14d2c7155f0176147c4362dde3b688393a43
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-05 20:52:03 +0100

    Google Problem #45 : not yet with cents

---
+++ google_problems/problem_45.py
@@ -0,0 +1,5 @@
+"""This question was asked by Google.
+Find the minimum number of coins required to make n cents.
+You can use standard American denominations, that is, 1¢, 5¢, 10¢, and 25¢.
+For example, given n = 16, return 3 since we can make it with a 10¢, a 5¢, and a 1¢.
+"""
