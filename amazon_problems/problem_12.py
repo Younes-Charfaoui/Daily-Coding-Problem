c34e4a693e4de69d0b7d151a738a2b60ae7b387c
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-06 21:52:47 +0100

    Amazon Problem #12: final solution

--- amazon_problems/problem_12.py
+++ amazon_problems/problem_12.py
@@ -5,3 +5,19 @@ A bit array is a space efficient array that holds a value of 1 or 0 at each inde
 •	set(i, val): updates index at i with val where val is either 1 or 0.
 •	get(i): gets the value at index i.
 """
+
+class BitArray:
+
+	def __init__(self, size):
+		self.data=[]
+		self.size = size
+		for i in range(size):
+			self.data.append(0)
+		
+	def set(i , val):
+		if 0 <= size < self.size
+			self.data[i] = val
+
+	def get(i):
+		if 0 <= size < self.size
+			return data[i]
