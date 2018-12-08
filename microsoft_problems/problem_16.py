7ff496db2a156baabda8c91cbceed83df18be283
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-08 20:03:19 +0100

    Microsoft Problem #16: stacks again, not yet

---
+++ microsoft_problems/problem_16.py
@@ -0,0 +1,14 @@
+"""This problem was asked by Microsoft.
+
+Implement 3 stacks using a single list:
+
+class Stack:
+    def __init__(self):
+        self.list = []
+
+    def pop(self, stack_number):
+        pass
+
+    def push(self, item, stack_number):
+        pass
+"""
