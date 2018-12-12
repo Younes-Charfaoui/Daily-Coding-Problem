0ac828f55e818d2cccda0cac0bc7a1342fc18f6e
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-12 18:12:29 +0100

    Google Problem #49: nothing yet

---
+++ google_problems/problem_49.py
@@ -0,0 +1,4 @@
+"""This problem was asked by Google.
+Given the head of a singly linked list, swap every two nodes and return its head.
+For example, given 1 -> 2 -> 3 -> 4, return 2 -> 1 -> 4 -> 3.
+"""
