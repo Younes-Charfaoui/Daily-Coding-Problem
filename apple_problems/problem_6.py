56377f61918a9cd8da5f7ff4f061bbc92ec885b7
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-15 18:38:05 +0100

    Apple Problem #6: not yet

---
+++ apple_problems/problem_6.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Apple.
+Gray code is a binary code where each successive value differ in only one bit, 
+as well as when wrapping around. 
+Gray code is common in hardware so that we don't see temporary spurious values during transitions.
+Given a number of bits n, generate a possible gray code for it.
+For example, for n = 2, one gray code would be [00, 01, 11, 10].
+"""
