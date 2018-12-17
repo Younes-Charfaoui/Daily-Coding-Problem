99851717fbda688dceedde9492ff5da896ce92a1
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-17 20:00:04 +0100

    Linkedin Problem #3 : not yet honney

---
+++ linked_in_problems/problem_3.py
@@ -0,0 +1,6 @@
+"""This problem was asked by LinkedIn.
+Given a list of points, a central point, and an integer k, find the nearest k points from the central point.
+For example, given the list of points [(0, 0), (5, 4), (3, 1)], the central point (1, 2), 
+and k = 2, return [(0, 0), (3, 1)].
+
+"""
