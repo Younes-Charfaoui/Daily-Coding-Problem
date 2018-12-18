dc60cbaf85eaefda14a94ded17be3ce4723b6a2e
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-18 19:14:27 +0100

    Other Problem #04: not yet

---
+++ others_problems/problem_4.py
@@ -0,0 +1,13 @@
+"""Given a 2-D matrix representing an image, a location of a pixel in the screen and a color C, 
+replace the color of the given pixel and all adjacent same colored pixels with C.
+For example, given the following matrix, and location pixel of (2, 2), and 'G' for green:
+B B W
+W W W
+W W W
+B B B
+Becomes
+B B G
+G G G
+G G G
+B B B
+"""
