56d751d8899581036392c6ce19a8e347d579e9f8
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-20 23:09:43 +0100

    Others problem #5: not yet

---
+++ others_problems/problem_5.py
@@ -0,0 +1,6 @@
+"""Find an efficient algorithm to find the smallest distance 
+(measured in number of words) between any two given words in a string.
+For example, given words "hello", and "world" and a text 
+content of "dog cat hello cat dog dog hello cat world", return 1 because 
+there's only one word "cat" in between the two words.
+"""
