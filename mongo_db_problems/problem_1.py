4fca6059139000def28c432016ce57e9040892e4
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-22 21:10:23 +0100

    MongoDB Problem #1: NoSQL comes into playing -_-

---
+++ mongo_db_problems/problem_1.py
@@ -0,0 +1,8 @@
+"""This problem was asked by MongoDB.
+
+Given a list of elements, find the majority element, 
+which appears more than half the times (> floor(len(lst) / 2.0)).
+
+You can assume that such element exists.
+For example, given [1, 2, 1, 1, 3, 4, 0], return 1.
+"""
