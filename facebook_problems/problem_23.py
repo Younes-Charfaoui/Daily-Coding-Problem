a0a93152f8df61f2c86e4cd99928cec94f9f2c62
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-23 19:08:57 +0100

    Facebook problem #23: not yet

---
+++ facebook_problems/problem_23.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Facebook.
+Given a positive integer n, find the smallest number of squared integers which sum to n.
+For example, given n = 13, return 2 since 13 = 3^2 + 2^2 = 9 + 4.
+Given n = 27, return 3 since 27 = 3^2 + 3^2 + 3^2 = 9 + 9 + 9.
+"""
