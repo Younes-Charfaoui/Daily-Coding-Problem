b5b0f39ad862c877074f1abb16079287cf8c5444
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-24 19:36:26 +0100

    Amazon Problem #15; not yet

---
+++ amazon_problems/problem_15.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Amazon.
+
+Given a string, determine whether any permutation of it is a palindrome.
+
+For example, carrace should return true, 
+since it can be rearranged to form racecar, which is a palindrome. 
+daily should return false, since there's no rearrangement that can form a palindrome.
+"""
