d03a0a9fdb2d69d58e76831dd4e5766ea1148389
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-25 22:17:10 +0100

    Slack Problem #1: slack start with dirty hands :D

---
+++ slack_problems/problem_1.py
@@ -0,0 +1,18 @@
+"""This problem was asked by Slack.
+
+You are given an N by M matrix of 0s and 1s. Starting from the top left corner, 
+how many ways are there to reach the bottom right corner?
+
+You can only move right and down. 0 represents an empty space while 1 represents a wall you cannot walk through.
+
+For example, given the following matrix:
+
+[[0, 0, 1],
+ [0, 0, 1],
+ [1, 0, 0]]
+Return two, as there are only two ways to get to the bottom right:
+
+Right, down, down, right
+Down, right, down, right
+The top left corner and bottom right corner will always be 0.
+"""
