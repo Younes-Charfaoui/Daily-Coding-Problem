1ec109c7bb37da81c19ad875750a370ba6f7d3cf
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-26 20:50:18 +0100

    Google Problem #50: not yet

---
+++ google_problems/problem_50.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Google.
+
+Given a string, return the first recurring character in it, or null if there is no recurring chracter.
+
+For example, given the string "acbbac", return "b". Given the string "abcdef", return null.
+"""
