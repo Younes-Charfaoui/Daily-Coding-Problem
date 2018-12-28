4397f71192ae311899616a98c0f2b46ae876b118
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-28 19:07:11 +0100

    Facebook Problem #24: not yet :D

---
+++ facebook_problems/problem_24.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Facebook.
+
+Given a 32-bit integer, return the number with its bits reversed.
+
+For example, given the binary number 1111 0000 1111 0000 1111 0000 1111 0000, 
+return 0000 1111 0000 1111 0000 1111 0000 1111.
+"""
