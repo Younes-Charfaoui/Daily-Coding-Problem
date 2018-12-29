1ec676ac602b89f547bcd938e8c9ef2c0e4e0cde
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-29 18:24:52 +0100

    Square Problem #3: not yet again

---
+++ square_problems/problem_3.py
@@ -0,0 +1,14 @@
+"""This problem was asked by Square.
+Given a list of words, return the shortest unique prefix of each word. For example, given the list:
+•	dog
+•	cat
+•	apple
+•	apricot
+•	fish
+Return the list:
+•	d
+•	c
+•	app
+•	apr
+•	f
+"""
