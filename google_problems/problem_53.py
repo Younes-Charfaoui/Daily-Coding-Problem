0f94f82bb52c04c09038772503285c01591f9265
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-05 18:23:12 +0100

    Google Problem #53: not yet, linked list again

---
+++ google_problems/problem_53.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Google.
+
+Given a linked list, sort it in O(n log n) time and constant space.
+
+For example, the linked list 4 -> 1 -> -3 -> 99 should become -3 -> 1 -> 4 -> 99.
+"""
