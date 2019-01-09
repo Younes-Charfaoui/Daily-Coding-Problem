2f79060dacb194c7f0330d3f7f0253dcb7e1d5fd
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-09 20:14:26 +0100

    Stripe Problem #3: not yet :D

---
+++ stripe_problems/problem_3.py
@@ -0,0 +1,24 @@
+"""This problem was asked by Stripe.
+
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+You can assume keys do not contain dots in them, i.e. no clobbering will occur.
+"""
