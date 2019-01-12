cce4a077a9968b0efde9764586d8e43115c0136a
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-12 20:28:50 +0100

    Bloomebreg Problem #1: yet another company come in, not yet

---
+++ bloomberg_problems/problem_1.py
@@ -0,0 +1,11 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Bloomberg.
+
+Determine whether there exists a one-to-one character mapping from one string s1 to another s2.
+
+For example, given s1 = abc and s2 = bcd, return true since we can map a to b, b to c, and c to d.
+
+Given s1 = foo and s2 = bar, return false since the o cannot map to two characters.
+"""
