938fa16250abbe54ed92e0c2b3b2feed9489aba3
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-13 18:20:26 +0100

    AirBnb Problem #4 : not yet, dealing with linked list problems

---
+++ airbnb_problems/problem_4.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Airbnb.
+Given a linked list and a positive integer k, rotate the list to the right by k places.
+For example, given the linked list 7 -> 7 -> 3 -> 5 and k = 2, it should become 3 -> 5 -> 7 -> 7.
+Given the linked list 1 -> 2 -> 3 -> 4 -> 5 and k = 3, it should become 3 -> 4 -> 5 -> 1 -> 2
+"""
