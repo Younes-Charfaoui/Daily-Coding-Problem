5d98758cdb8eafcc943385f647df0407c1bf10dd
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-15 21:22:57 +0100

    Google Problem #55: not yet

---
+++ google_problems/problem_55.py
@@ -0,0 +1,12 @@
+"""This problem was asked by Google.
+
+Given the sequence of keys visited by a postorder traversal of a binary search tree, reconstruct the tree.
+
+For example, given the sequence 2, 4, 3, 8, 7, 5, you should construct the following tree:
+
+    5
+   / \
+  3   7
+ / \\   \
+2   4   8
+"""
