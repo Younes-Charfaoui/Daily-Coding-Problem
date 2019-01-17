41fb0d6b46b575ae8c8b44e2cd2b42f9b7508e0d
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-17 20:42:03 +0100

    Google Problem #57: not yet like always

---
+++ google_problems/problem_57.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Google.
+
+Given a string, split it into as few strings as possible such that each string is a palindrome.
+
+For example, given the input string racecarannakayak, return ["racecar", "anna", "kayak"].
+
+Given the input string abc, return ["a", "b", "c"].
+"""
