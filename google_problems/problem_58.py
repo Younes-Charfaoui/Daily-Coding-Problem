3757d8a110ae55ccc0ba470e0264e9950e01018b
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-21 20:18:52 +0100

    Google Problem #58: not yet

---
+++ google_problems/problem_58.py
@@ -0,0 +1,15 @@
+"""This problem was asked by Google.
+Given two rectangles on a 2D graph, return the area of their intersection. 
+If the rectangles don't intersect, return 0.
+For example, given the following rectangles:
+{
+    "top_left": (1, 4),
+    "dimensions": (3, 3) # width, height
+}
+and
+{
+    "top_left": (0, 5),
+    "dimensions" (4, 3) # width, height
+}
+return 6.
+"""
