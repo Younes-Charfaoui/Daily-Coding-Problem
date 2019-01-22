3a88f0d57c26774558fb5d9614a91cceea33f6cc
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-22 18:12:46 +0100

    Microsoft Problem #18: not yet

---
+++ microsoft_problems/problem_18.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Microsoft.
+
+Given an array of positive integers, divide the array into two subsets such 
+that the difference between the sum of the subsets is as small as possible.
+
+For example, given [5, 10, 15, 20, 25], return the sets {10, 25} and {5, 15, 20}, 
+which has a difference of 5, which is the smallest possible difference.
+"""
