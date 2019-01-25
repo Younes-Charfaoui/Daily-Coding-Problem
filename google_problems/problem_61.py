4f6b4e6101caf3fcb7a1ad149a9ea9daf32f6d35
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-25 21:27:10 +0100

    Google Problem #61: not yet

---
+++ google_problems/problem_61.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Google.
+Given an array of elements, return the length of the longest
+subarray where all its elements are distinct.
+For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], 
+return 5 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
+"""
