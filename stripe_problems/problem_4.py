5ce527c53516d832f4318e7068d7829e8ca1acc6
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-27 21:56:24 +0100

    Stripe Problem #4: not yet

---
+++ stripe_problems/problem_4.py
@@ -0,0 +1,12 @@
+"""This problem was asked by Stripe.
+
+Given a collection of intervals, find the minimum number of intervals you 
+need to remove to make the rest of the intervals non-overlapping.
+
+Intervals can "touch", such as [0, 1] and [1, 2], but they won't be considered overlapping.
+
+For example, given the intervals (7, 9), (2, 4), (5, 8), return 1 as the 
+last interval can be removed and the first two won't overlap.
+
+The intervals are not necessarily sorted in any order.
+"""
