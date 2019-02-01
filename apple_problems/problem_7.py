8e3ed6f02ce4c7df82031046cebe6781575de43c
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-01 20:40:09 +0100

    Apple Problem #7: not yet

---
+++ apple_problems/problem_7.py
@@ -0,0 +1,12 @@
+"""This problem was asked by Apple.
+
+Given the root of a binary tree, find the most frequent subtree sum. 
+The subtree sum of a node is the sum of all values under a node, including the node itself.
+
+For example, given the following tree:
+
+  5
+ / \
+2  -5
+Return 2 as it occurs twice: once as the left leaf, and once as the sum of 2 + 5 - 5.
+"""
