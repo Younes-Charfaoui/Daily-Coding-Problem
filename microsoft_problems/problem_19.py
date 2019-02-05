b543c03142b7cf68529ab70cc0f87e1b5570b9c5
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-05 18:56:41 +0100

    Microsoft Problem #19 : not yet like always.

---
+++ microsoft_problems/problem_19.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Microsoft.
+
+Let X be a set of n intervals on the real line. We say that a set of points P "stabs" 
+X if every interval in X contains at least one point in P. 
+Compute the smallest set of points that stabs X.
+
+For example, given the intervals [(1, 4), (4, 5), (7, 9), (9, 12)], you should return [4, 9].
+"""
