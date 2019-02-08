7e330374d84a8ee632c5f98341b4ee726edf2fb4
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-08 19:19:01 +0100

    Uber Problem #5 : not yet.

---
+++ uber_problems/problem_5.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Uber.
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. 
+Find the minimum element in O(log N) time. You may assume the array does not contain duplicates.
+For example, given [5, 7, 10, 3, 4], return 3.
+"""
