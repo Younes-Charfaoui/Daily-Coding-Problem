061ce69b6e0aaac8b5b8c3e4300674c4c6b2ebd3
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-09 20:09:41 +0100

    Amazon Problem #19: not yet

---
+++ amazon_problems/problem_19.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Amazon.
+Given a complete binary tree, count the number of nodes in faster than O(n) time. 
+Recall that a complete binary tree has every level filled except the last, 
+and the nodes in the last level are filled starting from the left.
+"""
