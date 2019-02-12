7699e95fb40e431ca2104ef1883988fe3a28e418
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-12 19:46:56 +0100

    Dropbox Problem #6: not yet.

---
+++ dropbox_problem/problem_6.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Dropbox.
+
+Given an undirected graph G, check whether it is bipartite. 
+Recall that a graph is bipartite if its vertices can be divided into two 
+independent sets, U and V, such that no edge connects vertices of the same set."""
