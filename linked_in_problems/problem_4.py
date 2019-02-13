2e04eca86ad45645a33fa5f19eb8721f88eb993c
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-13 23:19:01 +0100

    LinkedIn Problem #4: not yet

---
+++ linked_in_problems/problem_4.py
@@ -0,0 +1,11 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by LinkedIn.
+
+Given a linked list of numbers and a pivot k, partition the linked list so that 
+all nodes less than k come before nodes greater than or equal to k.
+
+For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3 and k = 3, the 
+solution could be 1 -> 0 -> 5 -> 8 -> 3.
+"""
