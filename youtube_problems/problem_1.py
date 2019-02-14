b645263a69973a41b7d36b8fa77e505897c33a6e
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-14 19:32:51 +0100

    Youtube Problem #1: new player again, this one is handy

---
+++ youtube_problems/problem_1.py
@@ -0,0 +1,6 @@
+"""This problem was asked by YouTube.
+Write a program that computes the length of the 
+longest common subsequence of three given strings. 
+For example, given "epidemiologist", "refrigeration", and "supercalifragilisticexpialodocious", 
+it should return 5, since the longest common subsequence is "eieio".
+"""
