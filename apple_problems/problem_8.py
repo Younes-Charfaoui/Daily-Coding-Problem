4da71062394f9ff0a2372a7fc0334b44b563d5a2
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-15 20:27:36 +0100

    Apple Problem #8: not yet like always.

---
+++ apple_problems/problem_8.py
@@ -0,0 +1,9 @@
+"""This problem was asked by Apple.
+A Collatz sequence in mathematics can be defined as follows. Starting with any positive integer:
+
+if n is even, the next number in the sequence is n / 2
+if n is odd, the next number in the sequence is 3n + 1
+It is conjectured that every such sequence eventually reaches the number 1. Test this conjecture.
+
+Bonus: What input n <= 1000000 gives the longest sequence?
+"""
