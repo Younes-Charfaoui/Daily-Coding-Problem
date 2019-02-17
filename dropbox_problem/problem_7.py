ec3ade1203a45fc14e3f9ac79e66d87b2b72d25a
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-17 19:24:10 +0100

    DropBox Problem #7: not yet

---
+++ dropbox_problem/problem_7.py
@@ -0,0 +1,7 @@
+"""TThis problem was asked by Dropbox.
+
+Spreadsheets often use this alphabetical encoding for its columns: 
+"A", "B", "C", ..., "AA", "AB", ..., "ZZ", "AAA", "AAB", ....
+
+Given a column number, return its alphabetical column id. 
+For example, given 1, return "A". Given 27, return "AA"."""
