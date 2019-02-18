60f342f7c46c8c7017c6dc60b9d27a0e7f309e97
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-18 20:20:23 +0100

    Snapchat problem #4: not yet

---
+++ snapchat_problems/problem_4.py
@@ -0,0 +1,9 @@
+"""This problem was asked by Snapchat.
+
+Given a string of digits, generate all possible valid IP address combinations.
+
+IP addresses must follow the format A.B.C.D, where A, B, C, and D are numbers between 0 and 255. 
+Zero-prefixed numbers, such as 01 and 065, are not allowed, except for 0 itself.
+
+For example, given "2542540123", you should return ['254.25.40.123', '254.254.0.123'].
+"""
