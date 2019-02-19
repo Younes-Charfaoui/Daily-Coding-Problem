bedd797738199cb1a54071d524184b68269d1377
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-19 18:55:29 +0100

    Stripe Problem #5: not yet

---
+++ stripe_problems/problem_5.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Stripe.
+
+Given an integer n, return the length of the longest 
+consecutive run of 1s in its binary representation.
+
+For example, given 156, you should return 3.
+"""
