9619d49be468fb98e790073f6c3fcd0afc46861a
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-20 20:15:10 +0100

    Yelp Problem #2: not yet like always

--- yelp_problems/problem_1.py
+++ yelp_problems/problem_1.py
@@ -1,4 +1,6 @@
-"""Given a mapping of digits to letters (as in a phone number), and a digit string, 
+"""This problem was asked by Yelp.
+
+Given a mapping of digits to letters (as in a phone number), and a digit string, 
 return all possible letters the number could represent. You can assume each 
 valid number in the mapping is a single digit.
 For example if {“2”: [“a”, “b”, “c”], 3: [“d”, “e”, “f”], …} 
