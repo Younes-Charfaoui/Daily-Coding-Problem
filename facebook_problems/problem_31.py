8cdc568a14d36855051b9ec6c1dbfcddb2c79c86
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-21 19:05:32 +0100

    Facebook Problem #31: not yet

---
+++ facebook_problems/problem_31.py
@@ -0,0 +1,19 @@
+"""This problem was asked by Facebook.
+
+Given a number in Roman numeral format, convert it to decimal.
+
+The values of Roman numerals are as follows:
+
+{
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1
+}
+In addition, note that the Roman numeral system uses subtractive notation for numbers such as IV and XL.
+
+For the input XIV, for instance, you should return 14.
+"""
