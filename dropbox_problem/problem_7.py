cdd03399f09ebd58e3cf204ff35c5360b52df3a8
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-28 23:48:25 +0100

    Palantir Problem #4: not yet

--- dropbox_problem/problem_7.py
+++ dropbox_problem/problem_7.py
@@ -5,3 +5,8 @@ Spreadsheets often use this alphabetical encoding for its columns:
 
 Given a column number, return its alphabetical column id. 
 For example, given 1, return "A". Given 27, return "AA"."""
+
+def spreadsheet(number):
+    return number//26 + 1
+
+spreadsheet(27)	
