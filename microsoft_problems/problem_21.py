42a5dbfee5df979f6383dcbf845d9cfc173a7169
Younes Charfaoui <mxcsyounes@gmail.com>
2019-03-11 18:25:14 +0100

    Microsoft Problem #21: not yet

---
+++ microsoft_problems/problem_21.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Microsoft.
+
+Recall that the minimum spanning tree is the subset of edges of a tree that connect all 
+its vertices with the smallest possible total edge weight. 
+Given an undirected graph with weighted edges, compute the maximum weight spanning tree.
+"""
