cc3ea51bbcee4b97427c5468f33036286ef6c15a
Younes Charfaoui <mxcsyounes@gmail.com>
2019-03-12 19:20:33 +0100

    Facebook Problem #33: not yet

--- apple_problems/problem_9.py
+++
@@ -1,6 +0,0 @@
-"""This problem was asked by Apple.
-
-Implement the function fib(n), which returns the nth number 
-in the Fibonacci sequence, using only O(1) space.
-
-"""
---
+++ facebook_problems/problem_33.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Facebook.
+
+Given an array of numbers of length N, find both the 
+minimum and maximum using less than 2 * (N - 2) comparisons.
+"""
