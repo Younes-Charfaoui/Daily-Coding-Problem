3cd501f4583f85f63e46fe9c8726f7c10b390e12
Younes Charfaoui <mxcsyounes@gmail.com>
2019-03-20 17:10:28 +0100

    Etsy Problem #1: not player, new lot of not yet Hahahaha

---
+++ etsy_problems/problem_1.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Etsy.
+
+Given an array of numbers N and an integer k, your task is to split N into k 
+partitions such that the maximum sum of any partition is minimized. Return this sum.
+
+For example, given N = [5, 1, 2, 7, 3, 4] and k = 3, you should return 8, since the optimal 
+partition is [5, 1, 2], [7], [3, 4].
+"""
