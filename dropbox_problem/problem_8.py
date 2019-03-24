9e2b71335942ec916c38f65cff448c6e90563eed
Younes Charfaoui <mxcsyounes@gmail.com>
2019-03-24 20:46:45 +0100

    Dropbox Problem #8: not yet

---
+++ dropbox_problem/problem_8.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Dropbox.
+
+Given a list of words, determine whether the words can be chained to form a circle. 
+A word X can be placed in front of another word Y in a circle if the last character 
+of X is same as the first character of Y.
+
+For example, the words ['chair', 'height', 'racket', touch', 'tunic'] can form the 
+following circle: chair --> racket --> touch --> height --> tunic --> chair."""
