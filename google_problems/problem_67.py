f9a9e5e98b3606094319aaf63326fe240ce44b66
Younes Charfaoui <mxcsyounes@gmail.com>
2019-03-27 19:38:38 +0100

    Google Problem #67: google again, not yet

---
+++ google_problems/problem_67.py
@@ -0,0 +1,15 @@
+"""This problem was asked by Google.
+
+A cryptarithmetic puzzle is a mathematical game where the digits of some numbers are represented by letters.
+Each letter represents a unique digit.
+
+For example, a puzzle of the form:
+
+  SEND
++ MORE
+--------
+ MONEY
+may have the solution:
+
+{'S': 9, 'E': 5, 'N': 6, 'D': 7, 'M': 1, 'O', 0, 'R': 8, 'Y': 2}
+"""
