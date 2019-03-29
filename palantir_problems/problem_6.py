ece9e153cfc116b536b345cbc9fd1055745de497
Younes Charfaoui <mxcsyounes@gmail.com>
2019-03-29 18:50:42 +0100

    Palantir Problem #6: not yet

---
+++ palantir_problems/problem_6.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Palantir.
+
+The ancient Egyptians used to express fractions as a sum of several terms where each numerator is one. 
+For example, 4 / 13 can be represented as 1 / 4 + 1 / 18 + 1 / 468.
+
+Create an algorithm to turn an ordinary fraction a / b, where a < b, into an Egyptian fraction.
+"""
