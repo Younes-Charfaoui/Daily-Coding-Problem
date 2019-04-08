3b92c6d8716dd67553126ba75f405cbb5418e25b
Younes Charfaoui <mxcsyounes@gmail.com>
2019-04-08 17:22:58 +0100

    Mozila Problem #1: not yet

---
+++ mozila_problems/problem_1.py
@@ -0,0 +1,6 @@
+"""This problem was asked by Mozilla.
+
+A bridge in a connected (undirected) graph is an edge that, 
+if removed, causes the graph to become disconnected. 
+Find all the bridges in a graph.
+"""
