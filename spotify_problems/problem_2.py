55ec7da25228b65f958d545488f58d132e7f60d8
Younes Charfaoui <mxcsyounes@gmail.com>
2019-04-18 17:07:02 +0100

    Spotify Problem #2: not yet

---
+++ spotify_problems/problem_2.py
@@ -0,0 +1,8 @@
+"""
+This problem was asked by Spotify.
+
+Write a function, throw_dice(N, faces, total), that determines how many ways it is possible to 
+throw N dice with some number of faces each to get a specific total.
+
+For example, throw_dice(3, 6, 7) should equal 15.
+"""
