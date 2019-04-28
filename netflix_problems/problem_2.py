4b478c93c2354fdc08d7932b4e13e592b970e9ae
Younes Charfaoui <mxcsyounes@gmail.com>
2019-04-28 16:46:41 +0100

    Netflix Problem #2: not yet, Pythagorean Theorem again -_-

---
+++ netflix_problems/problem_2.py
@@ -0,0 +1,5 @@
+"""This problem was asked by Netflix.
+
+Given an array of integers, determine whether it contains a Pythagorean triplet. 
+Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c2.
+"""
