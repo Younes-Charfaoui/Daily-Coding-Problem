9b56f6c40bbef3947a605acb1c10cf3fcf268ec7
Younes Charfaoui <mxcsyounes@gmail.com>
2019-05-19 22:18:34 +0100

    Microsoft Problem #24: not yet

---
+++ microsoft_problems/problem_24.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Microsoft.
+
+Given a clock time in hh:mm format, determine, to the nearest degree, the angle between the 
+hour and the minute hands.
+
+Bonus: When, during the course of a day, will the angle be zero?
+"""
