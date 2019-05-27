4127f494ffa6e8ac69efe0d619607f533a60a3de
Younes Charfaoui <mxcsyounes@gmail.com>
2019-05-27 18:37:30 +0100

    Sumo Logic Problem #1: not yet, again new player -_-

---
+++ sumo_logic_problems/problem_1.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Sumo Logic.
+
+Given an unsorted array, in which all elements are distinct, find a "peak" element in O(log N) time.
+
+An element is considered a peak if it is greater than both its left and right neighbors. 
+It is guaranteed that the first and last elements are lower than all others.
+"""
