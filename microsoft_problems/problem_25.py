8c2aa621bc9ae8a342b1900f65fa99988a6a2d41
Younes Charfaoui <mxcsyounes@gmail.com>
2019-06-21 17:26:08 +0100

    Microsoft Problem #25: not yet honey

---
+++ microsoft_problems/problem_25.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Microsoft.
+
+Write a program to determine how many distinct ways there are to create a max heap 
+from a list of N given integers.
+
+For example, if N = 3, and our integers are [1, 2, 3], there are two ways, shown below.
+
+  3      3
+ / \    / \
+1   2  2   1
+"""
