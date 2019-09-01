5b47008c5c0f6fc75ad5e1af0a686f1f244fe714
Younes Charfaoui <mxcsyounes@gmail.com>
2019-09-01 23:12:50 +0100

    fixing the typo

--- others_problems/problem_6.py
+++ others_problems/problem_6.py
@@ -1,5 +1,4 @@
 """A number is considered deficient if the sum of its factor is less than twice that
 number. write a program to verify whether a given number is deficient or not.
-
-Bonus: write a program to  find all the deficient number in a range.
+Bonus: write a program to  find all the deficient number in a range. 
 """
