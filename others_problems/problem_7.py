10907e1bdb0d97b3971b1260705cf3ac55a8a53c
Younes Charfaoui <mxcsyounes@gmail.com>
2019-09-15 18:59:10 +0100

    adding new problem

---
+++ others_problems/problem_7.py
@@ -0,0 +1,19 @@
+"""Lily has a chocolate bar that she wants to share it with Ron for his birthday. Each of the squares has an integer on it. She decides to share a contiguous segment of the bar selected such that the length of the segment matches Ron's birth month and the sum of the integers on the squares is equal to his birth day. You must determine how many ways she can divide the chocolate.
+
+Consider the chocolate bar as an array of squares, . She wants to find segments summing to Ron's birth day,  with a length equalling his birth month, . In this case, there are two segments meeting her criteria:  and .
+
+Function Description
+
+Complete the birthday function in the editor below. It should return an integer denoting the number of ways Lily can divide the chocolate bar.
+
+birthday has the following parameter(s):
+
+s: an array of integers, the numbers on each of the squares of chocolate
+d: an integer, Ron's birth day
+m: an integer, Ron's birth month
+Input Format
+
+The first line contains an integer , the number of squares in the chocolate bar.
+The second line contains  space-separated integers , the numbers on the chocolate squares where .
+The third line contains two space-separated integers,  and , Ron's birth day and his birth month.
+"""
