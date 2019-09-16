a71280944aa02f0937d7f0248e4282cbdbfb7848
Younes Charfaoui <mxcsyounes@gmail.com>
2019-09-16 22:55:29 +0100

    Fibonacci Problem

---
+++ others_problems/problem_8.py
@@ -0,0 +1,4 @@
+"""You are given an integer, . Write a program to determine if  is an element of the Fibonacci sequence.
+
+The first few elements of the Fibonacci sequence are . A Fibonacci sequence is one where every element is a sum of the previous two elements in the sequence. The first two elements are  and .
+"""
