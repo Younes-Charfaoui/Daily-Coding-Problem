26c45e7ab3dbcbaa388d388db95c2c7cc7686580
Younes Charfaoui <mxcsyounes@gmail.com>
2019-11-23 19:41:32 +0100

    Two Sigma Problem #7: not yet

---
+++ two_sigma_problems/problem_7.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Two Sigma.
+
+You’re tracking stock price at a given instance of time. 
+Implement an API with the following functions: add(), update(), remove(), 
+which adds/updates/removes a datapoint for the stock price you are tracking. 
+The data is given as (timestamp, price), where timestamp is specified in unix
+epoch time.
+
+Also, provide max(), min(), and average() functions that give the max/min/average 
+of all values seen thus far.
+"""
