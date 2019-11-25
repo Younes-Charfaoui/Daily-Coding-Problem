91c5f658bfa7287b926f38506d3306f417b7f666
Younes Charfaoui <mxcsyounes@gmail.com>
2019-11-25 19:06:03 +0100

    Google Problem #81: not yet babe

---
+++ google_problems/problem_81.py
@@ -0,0 +1,17 @@
+"""This problem was asked by Google.
+
+You are given a series of arithmetic equations as a string, such as:
+
+y = x + 1
+5 = x + 3
+10 = z + y + 2
+The equations use addition only and are separated by newlines. 
+Return a mapping of all variables to their values. 
+If it's not possible, then return null. In this example, you should return:
+
+{
+  x: 2,
+  y: 3,
+  z: 5
+}
+"""
