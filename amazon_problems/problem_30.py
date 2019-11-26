0b18640e52b832068606778f0a3681dc39ae90ca
Younes Charfaoui <mxcsyounes@gmail.com>
2019-11-26 21:22:37 +0100

    Amazon Problem #30: not yet

---
+++ amazon_problems/problem_30.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Amazon.
+
+Write a function that takes a natural number as input 
+and returns the number of digits the input has.
+
+Constraint: don't use any loops.
+"""
