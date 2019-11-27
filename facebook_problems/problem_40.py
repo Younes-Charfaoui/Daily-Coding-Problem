8d43a9cbbe9e922dfca5b55f087ad35c6258637b
Younes Charfaoui <mxcsyounes@gmail.com>
2019-11-27 20:44:53 +0100

    Facebook Problem #40: not yet

---
+++ facebook_problems/problem_40.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Facebook.
+
+Given a list of integers L, find the maximum length of a sequence of consecutive 
+numbers that can be formed using elements from L.
+
+For example, given L = [5, 2, 99, 3, 4, 1, 100], return 5 as we 
+can build a sequence [1, 2, 3, 4, 5] which has length 5.
+"""
