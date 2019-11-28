8836861a63dd78dafe3d28ae47df0b493e19a101
Younes Charfaoui <mxcsyounes@gmail.com>
2019-11-28 21:39:18 +0100

    Amazon Problem #31: not yet

---
+++ amazon_problems/problem_31.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Amazon.
+
+Given a sorted array arr of distinct integers, 
+return the lowest index i for which arr[i] == i. 
+
+Return null if there is no such index.
+
+For example, given the array [-5, -3, 2, 3], 
+return 2 since arr[2] == 2. Even though arr[3] == 3, 
+we return 2 since it's the lowest index.
+"""
