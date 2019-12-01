4859f528f002be52eb08ba655a57ad86d390c1c0
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-01 19:11:04 +0100

    Microsoft Problem #27: not yet

---
+++ microsoft_problems/problem_27.py
@@ -0,0 +1,19 @@
+"""This problem was asked by Microsoft.
+
+Given an array of numbers arr and a window of size k, print out the median of 
+each window of size k starting from the left and moving right by one position each time.
+
+For example, given the following array and k = 3:
+
+[-1, 5, 13, 8, 2, 3, 3, 1]
+Your function should print out the following:
+
+5 <- median of [-1, 5, 13]
+8 <- median of [5, 13, 8]
+8 <- median of [13, 8, 2]
+3 <- median of [8, 2, 3]
+3 <- median of [2, 3, 3]
+3 <- median of [3, 3, 1]
+Recall that the median of an even-sized list is the average of the two middle numbers.
+
+"""
