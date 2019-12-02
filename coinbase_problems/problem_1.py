d19f6b076ec87eb8c6710b7a24b83b49b8c80617
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-02 18:48:30 +0100

    Coinebase Problem #1: new one joining our party

---
+++ coinbase_problems/problem_1.py
@@ -0,0 +1,12 @@
+"""This problem was asked by Coinbase.
+
+Write a function that takes in a number, string, list, or dictionary and 
+returns its JSON encoding. It should also handle nulls.
+
+For example, given the following input:
+
+[None, 123, ["a", "b"], {"c":"d"}]
+You should return the following, as a string:
+
+'[null, 123, ["a", "b"], {"c": "d"}]'
+"""
