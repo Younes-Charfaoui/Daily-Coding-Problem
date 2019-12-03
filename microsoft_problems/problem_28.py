a5a6fef4a0e32e4c6d6e471d13ce49eef33252dc
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-03 19:20:28 +0100

    Microsoft Problem #28: not yet like always.

---
+++ microsoft_problems/problem_28.py
@@ -0,0 +1,17 @@
+"""This problem was asked by Microsoft.
+
+Given a string, generate all possible subsequences of the string.
+
+For example, given the string xyz, return an array or set with the following strings:
+
+
+x
+y
+z
+xy
+xz
+yz
+xyz
+
+Note that zx is not a valid subsequence since it is not in the order of the given string.
+"""
