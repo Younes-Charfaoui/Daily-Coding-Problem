534996d8371993270e78670c41e5336a88deffcc
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-04 22:55:23 +0100

    NextDoor Problem #1: new player, not yet

---
+++ nextdoor_problems/problem_1.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Nextdoor.
+
+Implement integer division without using the division operator. 
+Your function should return a tuple of (dividend, remainder) and 
+it should take two numbers, the product and divisor.
+
+For example, calling divide(10, 3) should return (3, 1) since the 
+divisor is 3 and the remainder is 1.
+
+Bonus: Can you do it in O(log n) time?
+"""
