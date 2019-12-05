cd628d6d6267c7653a2411c40740472b32183068
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-05 21:33:35 +0100

    Paypal Problem #3: not yet.

---
+++ paypal_problems/problem_3.py
@@ -0,0 +1,13 @@
+"""This problem was asked by Paypal.
+
+Read this Wikipedia article on Base64 encoding.
+
+Implement a function that converts a hex string to base64.
+
+For example, the string:
+
+deadbeef
+should produce:
+
+3q2+7w==
+"""
