86716ca259df292ef0f94b1dd2ca2f9a82f248f2
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-06 19:10:28 +0100

    Google Problem #84: noot yet

---
+++ google_problems/problem_84.py
@@ -0,0 +1,13 @@
+"""This problem was asked by Google.
+
+Yesterday you implemented a function that encodes a hexadecimal string into Base64.
+
+Write a function to decode a Base64 string back to a hexadecimal string.
+
+For example, the following string:
+
+3q2+7w==
+should produce:
+
+deadbeef
+"""
