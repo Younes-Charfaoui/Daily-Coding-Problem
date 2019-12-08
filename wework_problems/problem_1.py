e580cb3e2411fd47fc87b13e6fbdd0007b994249
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-08 22:15:31 +0100

    WeWork Problem #1: not yet from DXB

---
+++ wework_problems/problem_1.py
@@ -0,0 +1,8 @@
+"""This problem was asked by WeWork.
+
+You are given an array of integers representing coin denominations and a total amount of money. Write a function to compute the fewest number of coins needed to make up that amount. If it is not possible to make that amount, return null.
+
+For example, given an array of [1, 5, 10] and an amount 56, return 7 since we can use 5 dimes, 1 nickel, and 1 penny.
+
+Given an array of [5, 8] and an amount 15, return 3 since we can use 5 5-cent coins.
+"""
