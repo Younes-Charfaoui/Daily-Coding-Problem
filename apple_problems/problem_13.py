9f82311574a19e7f79a41a7cbfacac84cd4a76e2
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-09 22:46:12 +0100

    Appele Problem #13: not yet

---
+++ apple_problems/problem_13.py
@@ -0,0 +1,11 @@
+"""This problem was asked by Apple.
+
+You are given a hexadecimal-encoded string that has been XOR'd against a single char.
+
+Decrypt the message. For example, given the string:
+
+7a575e5e5d12455d405e561254405d5f1276535b5e4b12715d565b5c551262405d505e575f
+You should be able to decrypt it and get:
+
+Hello world from Daily Coding Problem
+"""
