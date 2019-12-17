ad835e6e01b8b1454c62e00906a29a4dea049b8a
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-17 19:44:16 +0100

    AirBnb Problem #9: not yet

---
+++ airbnb_problems/problem_9.py
@@ -0,0 +1,7 @@
+"""This problem was asked by Airbnb.
+
+Given an array of integers, return the largest range, inclusive, 
+of integers that are all included in the array.
+
+For example, given the array [9, 6, 1, 3, 8, 10, 12, 11], 
+return (8, 12) since 8, 9, 10, 11, and 12 are all in the array."""
