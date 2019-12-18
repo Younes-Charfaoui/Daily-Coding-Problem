14b115d303c3b5ba2ec27efe69f466fcfef5023a
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-18 18:56:23 +0100

    Uber Problem #11: not yet

---
+++ uber_problems/problem_11.py
@@ -0,0 +1,14 @@
+"""This problem was asked by Uber.
+
+Given a binary tree and an integer k, return whether there exists a root-to-leaf
+path that sums up to k.
+
+For example, given k = 18 and the following binary tree:
+
+    8
+   / \
+  4   13
+ / \   \
+2   6   19
+Return True since the path 8 -> 4 -> 6 sums to 18.
+"""
