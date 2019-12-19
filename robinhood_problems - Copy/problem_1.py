ed2d5712e20dcb488396a22526fcbfa26d4e80ca
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-19 19:05:23 +0100

    RobinHood Problem #1: not yet like always.

---
+++ robinhood_problems - Copy/problem_1.py
@@ -0,0 +1,13 @@
+"""This problem was asked by Robinhood.
+
+Given an array of strings, group anagrams together.
+
+For example, given the following array:
+
+['eat', 'ate', 'apt', 'pat', 'tea', 'now']
+Return:
+
+[['eat', 'ate', 'tea'],
+ ['apt', 'pat'],
+ ['now']]
+"""
