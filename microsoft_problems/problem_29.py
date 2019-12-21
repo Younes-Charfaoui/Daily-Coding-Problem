ed519ef0cd8cd09ca35d07af5c09cc7fe12f9405
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-21 19:17:57 +0100

    Microsoft Problem #29 : not yet

---
+++ microsoft_problems/problem_29.py
@@ -0,0 +1,22 @@
+"""This problem was asked by Microsoft.
+
+You are given a list of jobs to be done, where each job is represented 
+by a start time and end time. Two jobs are compatible if they don't overlap. 
+Find the largest subset of compatible jobs.
+
+For example, given the following jobs (there is no guarantee that jobs will be sorted):
+
+[(0, 6),
+(1, 4),
+(3, 5),
+(3, 8),
+(4, 7),
+(5, 9),
+(6, 10),
+(8, 11)]
+Return:
+
+[(1, 4),
+(4, 7),
+(8, 11)]
+"""
