1185833879cc5e266c8c6a6a12450e40a07a21a5
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-22 22:26:02 +0100

    Amazon Problem #33: not yet babe.

---
+++ amazon_problems/problem_33.py
@@ -0,0 +1,9 @@
+"""This problem was asked by Amazon.
+
+Given a linked list and an integer k, remove the k-th node from the end of the 
+list and return the head of the list.
+
+k is guaranteed to be smaller than the length of the list.
+
+Do this in one pass.
+"""
