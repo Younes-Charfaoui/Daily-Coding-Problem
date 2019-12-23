c5ecfb4b0c457b1e301971714c41db1619a10ee1
Younes Charfaoui <mxcsyounes@gmail.com>
2019-12-23 20:24:57 +0100

    Facebook Problem #42: not yet

---
+++ facebook_problems/problem_42.py
@@ -0,0 +1,15 @@
+"""This problem was asked by Facebook.
+
+Given a list of strictly positive integers, partition the list into 3 contiguous 
+partitions which each sum up to the same value. If not possible, return null.
+
+For example, given the following list:
+
+[3, 5, 8, 0, 8]
+Return the following 3 partitions:
+
+[[3, 5],
+ [8, 0],
+ [8]]
+Which each add up to 8.
+"""
