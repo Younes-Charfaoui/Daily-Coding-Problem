91b9127773a6ee5221d8d6edd56e7ab809158a90
Younes Charfaoui <mxcsyounes@gmail.com>
2020-05-01 20:52:25 +0100

    Pinterest Problem #4: not yet.

---
+++ pinterest_problems/problem_4.py
@@ -0,0 +1,8 @@
+"""This problem was asked by Pinterest.
+
+
+Given a binary tree, write a function to determine whether the tree is a 
+mirror image of itself. 
+Two trees are a mirror image if their root values are the same and the 
+left subtree is a mirror image of the right subtree.
+"""
