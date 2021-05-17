ec38e4e18c56b78789a9287bf01a558e9b9141cc
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-17 06:15:10 -0700

    completed

--- google_problems/problem_1.py
+++ google_problems/problem_1.py
@@ -2,3 +2,23 @@
 Given a list of numbers and a number k, return whether any two numbers from the
 list add up to k. For example, given [10, 15, 3, 7] and k of 17, return true
 since 10 + 7 is 17. Bonus: Can you do this in one pass? """
+
+# BRUT FORCE SOLUTION
+#
+# arr = [10, 15, 3, 7]
+# for i in arr:
+#     for j in arr:
+#         if i + j == 17:
+#             print(True)
+
+# ONE PASS SOLUTION
+def check(arr, num):
+    for i in arr:
+        needed_num = num - i
+        if needed_num in arr:
+            return True
+
+if __name__ == '__main__':
+    arr = [10, 15, 3, 7]
+    checked = check(arr,17)
+    print(checked)
