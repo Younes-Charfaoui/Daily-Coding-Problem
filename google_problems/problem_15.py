42267db76771670cfbd21775e69631c8ea20888f
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-26 09:10:07 -0700

    first try

--- google_problems/problem_15.py
+++ google_problems/problem_15.py
@@ -7,3 +7,17 @@ For example, given [6, 1, 3, 3, 3, 6, 6],
 return 1. Given [13, 19, 13, 13], return 19.
 
 Do this in O(N) time and O(1) space."""
+
+# FIsrt try
+arr = [6, 1, 3, 3, 3, 6, 6]
+def get_duplicated(arr=arr):
+    dic = []
+    for i in arr:
+        if i in dic:
+            dic.remove(i)
+        else:
+            dic.append(i)
+    return dic
+
+arr = get_duplicated()
+print(arr)
